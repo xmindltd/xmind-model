3472c5985553ddc7d56514f82635f99a46ad9990
danielsss <better.sunjian@gmail.com>
2019-06-10 15:26:11 +0800

    added: simple test

--- test/index.ts
+++ test/index.ts
@@ -5,6 +5,15 @@ import * as Core from '../src';
 const wb = new Core.Workbook([{id: '1', sheetTitle: '123', rootTopic: {id: '2', title: 'roottopic'}}]);
 const sheet = wb.getSheetById('1');
 
-const topic = sheet.getRootTopic()
+const topic = sheet.getRootTopic();
 
 topic.addChildTopic({title: '123123', id: 'asdada-asdasdd-lkj-sdfg-dskfhsj'});
+
+console.info(wb.toString());
+
+console.info('\n\n');
+
+console.info(wb.toJSON());
+
+console.info('\n\n');
+console.info(wb.toJSON()[0].rootTopic.children);
