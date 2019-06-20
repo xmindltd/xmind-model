d846a8eaab9f93d6f585510d0eb2149e85f5159b
danielsss <better.sunjian@gmail.com>
2019-06-20 10:47:26 +0800

    fixed: export bug

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { default as Workbook } from './models/workbook'
 export { default as Sheet } from './models/sheet'
-export { default as Topic } from './models/Topic'
+export { default as Topic } from './models/topic'
 
 export { default as validator } from './validator'
